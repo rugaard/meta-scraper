70e4cafa3f889fccd9ddfa431c8a600fa52c7523
Morten Rugaard <morten@rugaard.me>
2016-12-27 15:03:24 +0100

    Refactored tests

--- tests/Traits/OpenGraph/OpenGraphTest.php
+++ tests/Traits/OpenGraph/OpenGraphTest.php
@@ -4,6 +4,7 @@ declare (strict_types = 1);
 namespace Tests\Traits\OpenGraph;
 
 use GuzzleHttp\Psr7\Response as GuzzleResponse;
+use Illuminate\Support\Collection;
 use Rugaard\MetaScraper\Traits\OpenGraph\OpenGraph;
 use Tests\AbstractTestCase;
 
@@ -18,6 +19,28 @@ class OpenGraphTest extends AbstractTestCase
      * @var \PHPUnit_Framework_MockObject_MockObject
      */
     protected $trait;
+
+    /**
+     * Test method [openGraph].
+     *
+     * @return void
+     */
+    public function testOpenGraph()
+    {
+        $mockedTrait = $this->createPartialMock(get_class($this->trait), ['getAllByNamespace']);
+        $mockedTrait->method('getAllByNamespace')->will($this->returnCallback(function ($namespace) {
+            return $this->invokeMethod($this->scraper, 'getAllByNamespace', [$namespace]);
+        }));
+
+        $data = $mockedTrait->openGraph();
+
+        $this->assertNotEmpty($data);
+        $this->assertArrayHasKey('title', $data);
+        $this->assertArrayHasKey('description', $data);
+        $this->assertArrayHasKey('image', $data);
+        $this->assertArrayHasKey('video', $data);
+        $this->assertArrayHasKey('audio', $data);
+    }
 
     /**
      * Test method [parseOpenGraphStandard].
@@ -37,11 +60,11 @@ class OpenGraphTest extends AbstractTestCase
     }
 
     /**
-     * Test method [parseOpenGraphMedia] with images.
+     * Test method [parseOpenGraphMedia].
      *
      * @return void
      */
-    public function testParseOpenGraphMediaImage()
+    public function testParseOpenGraphMedia()
     {
         $openGraphData = $this->invokeMethod($this->scraper, 'getAllByNamespace', ['og']);
         $this->invokeMethod($this->trait, 'parseOpenGraphMedia', [$openGraphData]);
@@ -49,61 +72,42 @@ class OpenGraphTest extends AbstractTestCase
         $data = $this->trait->getOpenGraph();
 
         $this->assertNotEmpty($data);
-        $this->assertArrayHasKey('image', $data);
 
+        $this->assertArrayHasKey('image', $data);
         $this->assertEquals('http://example.com/image.jpg', $data['image'][0]->getUrl());
         $this->assertEquals('https://example.com/image.jpg', $data['image'][0]->getSecureUrl());
         $this->assertEquals('image/jpeg', $data['image'][0]->getMimeType());
         $this->assertEquals(600, $data['image'][0]->getWidth());
         $this->assertEquals(315, $data['image'][0]->getHeight());
-
         $this->assertEquals('http://example.com/alternative.jpg', $data['image'][1]->getUrl());
-    }
 
-    /**
-     * Test method [parseOpenGraphMedia] with videos.
-     *
-     * @return void
-     */
-    public function testParseOpenGraphMediaVideos()
-    {
-        $openGraphData = $this->invokeMethod($this->scraper, 'getAllByNamespace', ['og']);
-        $this->invokeMethod($this->trait, 'parseOpenGraphMedia', [$openGraphData]);
-
-        $data = $this->trait->getOpenGraph();
-
-        $this->assertNotEmpty($data);
         $this->assertArrayHasKey('video', $data);
-
         $this->assertEquals('http://example.com/video.mp4', $data['video'][0]->getUrl());
         $this->assertEquals('https://example.com/video.mp4', $data['video'][0]->getSecureUrl());
         $this->assertEquals('video/mpeg', $data['video'][0]->getMimeType());
         $this->assertEquals(600, $data['video'][0]->getWidth());
         $this->assertEquals(400, $data['video'][0]->getHeight());
-
         $this->assertEquals('http://example.com/alternative.mp4', $data['video'][1]->getUrl());
-    }
 
-    /**
-     * Test method [parseOpenGraphMedia] with audios.
-     *
-     * @return void
-     */
-    public function testParseOpenGraphMediaAudios()
-    {
-        $openGraphData = $this->invokeMethod($this->scraper, 'getAllByNamespace', ['og']);
-        $this->invokeMethod($this->trait, 'parseOpenGraphMedia', [$openGraphData]);
-
-        $data = $this->trait->getOpenGraph();
-
-        $this->assertNotEmpty($data);
         $this->assertArrayHasKey('audio', $data);
-
         $this->assertEquals('http://example.com/audio.mp3', $data['audio'][0]->getUrl());
         $this->assertEquals('https://example.com/audio.mp3', $data['audio'][0]->getSecureUrl());
         $this->assertEquals('audio/mpeg', $data['audio'][0]->getMimeType());
-
         $this->assertEquals('http://example.com/alternative.mp3', $data['audio'][1]->getUrl());
+    }
+
+    /**
+     * Test method [parseOpenGraphMedia] can handle an empty Collection.
+     *
+     * @return void
+     */
+    public function testParseOpenGraphMediaIsEmpty()
+    {
+        $this->invokeMethod($this->trait, 'parseOpenGraphMedia', [new Collection]);
+
+        $data = $this->trait->getOpenGraph();
+
+        $this->assertEmpty($data);
     }
 
     /**
