e5cc9604998b84add58b3d3eebdf777c4ba3c67c
Morten Rugaard <morten@rugaard.me>
2016-12-28 10:55:25 +0100

    Added missing DocBlock and exceptions to AbstractObject

---
+++ src/Exceptions/MethodNotFoundException.php
@@ -0,0 +1,16 @@
+<?php
+declare (strict_types = 1);
+
+namespace Rugaard\MetaScraper\Exceptions;
+
+use Exception;
+
+/**
+ * Class MethodNotFoundException
+ *
+ * @package Rugaard\MetaScraper\Exceptions
+ */
+class MethodNotFoundException extends Exception
+{
+
+}
--- src/Traits/OpenGraph/Objects/AbstractObject.php
+++ src/Traits/OpenGraph/Objects/AbstractObject.php
@@ -4,6 +4,8 @@ declare (strict_types = 1);
 namespace Rugaard\MetaScraper\Traits\OpenGraph\Objects;
 
 use Illuminate\Support\Collection;
+use Rugaard\MetaScraper\Exceptions\AttributeNotFoundException;
+use Rugaard\MetaScraper\Exceptions\MethodNotFoundException;
 
 /**
  * Class AbstractObject.
@@ -34,15 +36,29 @@ abstract class AbstractObject
      */
     abstract public function parse(Collection $data);
 
+    /**
+     * __call.
+     *
+     * @param  string $name
+     * @param  array  $arguments
+     * @return mixed
+     * @throws \Rugaard\MetaScraper\Exceptions\MethodNotFoundException
+     * @throws \Rugaard\MetaScraper\Exceptions\AttributeNotFoundException
+     */
     public function __call($name, $arguments)
     {
+        // If $name does not start with "get"
+        // then we do not support the request.
         if (substr($name, 0, 3) != 'get') {
-            die('Method found');
+            throw new MethodNotFoundException((sprintf('Method [%s] not found.', $name), 500);
         }
 
+        // Convert name into a snake_case attribute.
         $attribute = strtolower(preg_replace('/(?<=\\w)(?=[A-Z])/',"_$1", substr($name, 3)));
+
+        // Make sure attribute exists.
         if (!array_key_exists($attribute, $this->attributes)) {
-            die('Attribute does not exist');
+            throw new AttributeNotFoundException(sprintf('Attribute [%s] not found.', $attribute), 500);
         }
 
         return $this->attributes[$attribute];
